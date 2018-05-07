62f5ab8a9ff6fd22cfd5bdab3edd1fb62e264b25
David-Zhuzhunashvili <david.zhuzhunashvili@colorado.edu>
2018-05-07 16:22:07 -0600

    updates every ms

--- js/analog-clock.js
+++ js/analog-clock.js
@@ -1,20 +1,25 @@
 const HOURHAND = document.querySelector('#hour')
 const MINUTEHAND = document.querySelector('#minute')
 const SECONDHAND = document.querySelector('#second')
+const MILLIHAND = document.querySelector('#milli')
 
 function clock() {
   var date = new Date()
   var hours = date.getHours()
   var minutes = date.getMinutes()
   var seconds = date.getSeconds()
+  var milli = date.getMilliseconds()
 
   var hourPosition = hours/12*360 + minutes/60*360/12
   var minutePosition = minutes/60*360 + seconds/60*360/60
   var secondPosition = seconds/60*360
+  var milliPosition = milli/1000*360
 
   HOURHAND.style.transform = `rotate(${hourPosition}deg)`
   MINUTEHAND.style.transform = `rotate(${minutePosition}deg)`
   SECONDHAND.style.transform = `rotate(${secondPosition}deg)`
+  MILLIHAND.style.transform = `rotate(${milliPosition}deg)`
+
 }
 
 function run() {
@@ -23,7 +28,7 @@ function run() {
     time = new Date()
   }
   clock()
-  setInterval(clock, 1000)
+  setInterval(clock, 1)
 }
 
 run()
